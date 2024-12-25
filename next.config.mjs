bf7bfae720870d3a4500150911a95f8e67964c07
Erik Toft <erikthoft@hotmail.com>
2024-12-25 13:42:39 +0100

    Prepare for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import path from "path";
+
+// Hämta basvägen för att hantera alias
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    // Lägg till alias för @
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"), // Mappas till ./src/
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
