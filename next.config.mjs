7b6cf682228a067fa46c096058e777571d026067
Erik Toft <erikthoft@hotmail.com>
2024-12-25 20:35:13 +0100

    changed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
-import path from "path";
-
-// Hämta basvägen för att hantera alias
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    // Lägg till alias för @
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"), // Mappas till ./src/
-    };
-    return config;
-  },
-  output: "export", // Lägg till detta för att möjliggöra statisk export
-  distDir: "out", // Mappen för de statiska filerna
-  trailingSlash: true, // För att säkerställa att URL:erna slutar med '/'
-};
+const nextConfig = {};
 
 export default nextConfig;
