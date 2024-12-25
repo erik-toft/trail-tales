ccfd5b6b0eabbcc9ca3a661c6f54e8a804b05cdb
Erik Toft <erikthoft@hotmail.com>
2024-12-25 13:21:44 +0100

    Prepare for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+const nextConfig = {
+  experimental: {
+    appDir: true, // Aktiverar appDir för att använda /app för sidor istället för /pages
+  },
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve("src"); // Sätter upp alias för @ till src-mappen
+    return config;
+  },
+};
 
 export default nextConfig;
